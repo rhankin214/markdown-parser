f4b3bbc414deabaa9b5fe64d3acc34485f43da27
rhankin214 <100500846+rhankin214@users.noreply.github.com>
2022-04-21 16:46:33 -0700

    Same as last commit. Changes weren't added properly.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
 
 import org.junit.*;
 public class MarkdownParseTest {
+    ArrayList<String> expectedLinks;
+    ArrayList<String> gottenLinks;
+    MarkdownParse mrkdwnPrse = new MarkdownParse();
     @Before
     public void setup()
     {
-        ArrayList<String> gottenLinks = new ArrayList<>();
-        ArrayList<String> expectedLinks = new ArrayList<>();
+        gottenLinks = new ArrayList<String>();
+        expectedLinks = new ArrayList<String>();
     }
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void getLinksTwo(){
-        MarkdownParse mrkdwnPrse = new MarkdownParse();
-        mrkdwnPrse.getLinks("test-file.md");
+    public void getLinksTestFile(){
+        expectedLinks.add("https://something.com");
+        expectedLinks.add("some-thing.html");
+        gottenLinks = mrkdwnPrse.getLinks("test-file.md");
+        for(int i = 0; i < expectedLinks.size(); i++)
+        {
+            assertEquals(i + "th link didn't match for test-file", expectedLinks.get(i), 
+                gottenLinks.get(i));
+        }
     }
 }
