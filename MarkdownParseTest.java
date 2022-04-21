d2bbbd62734567d4be36552ca7d9b9b82fef1ce5
rhankin214 <100500846+rhankin214@users.noreply.github.com>
2022-04-21 16:45:17 -0700

    added setup and one test method to MarkdownParseTester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,22 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.*;
 public class MarkdownParseTest {
+    @Before
+    public void setup()
+    {
+        ArrayList<String> gottenLinks = new ArrayList<>();
+        ArrayList<String> expectedLinks = new ArrayList<>();
+    }
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void getLinksTwo(){
+        MarkdownParse mrkdwnPrse = new MarkdownParse();
+        mrkdwnPrse.getLinks("test-file.md");
     }
 }
