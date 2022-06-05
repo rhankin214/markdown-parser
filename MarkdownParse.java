488e11deb70d8e4c06b4b015ff6d1365993e8ba0
rhankin214 <100500846+rhankin214@users.noreply.github.com>
2022-06-04 22:01:56 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -50,14 +50,8 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         System.out.println(Files.readString(fileName));
-        /*if(Files.isDirectory(fileName))
-        {
-            System.out.println(Files.readString(fileName));
-        }*/
-            String content = Files.readString(fileName);
+        String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
         System.out.println(links);
-        
-        //hello
     }
 }
